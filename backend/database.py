fd2839a3b16aac03fbf9b43c78b7e11d012af5fb
Aish <aish@Aghils-MacBook-Pro.local>
2025-08-19 16:24:30 +0530

    modifications so as to deploy on aws

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # You can change this to your actual database URL
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+is_sqlite = DATABASE_URL.startswith("sqlite")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if is_sqlite else {},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
